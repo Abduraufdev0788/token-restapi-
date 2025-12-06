1940dc00a49cbf63ca790a903393950e317bf29b
abdurauf0788 <turkeynumber063@gmail.com>
2025-12-06 17:38:24 +0500

    profile api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Register, Login, Logout
+from .views import Register, Login, Logout, Profile
 
 urlpatterns = [
     path('auth/register/', Register.as_view()),
     path('auth/login/', Login.as_view()),
     path('auth/logout/', Logout.as_view()),
+    path('auth/profile/', Profile.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -49,6 +49,17 @@ class Logout(APIView):
         request.user.auth_token.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class Profile(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request: Request)->Response:
+        user = request.user
+
+        serializer = UserSerializers(user)
+
+        return Response(serializer.data)
 
         
 
